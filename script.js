26f7e1db01d4ed0d29c6db2195cf46e827951067
ArnoldIngrave <jalyn.campbell@yahoo.com>
2020-11-19 17:18:07 -0500

    commit on main

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//var quizQuestions[]
+// Calling all elements
+const startBtn; = document.querySelector(".start-btn button");
+const debriefing; = document.querySelector(".debriefing");
+const exitBtn; = debriefing.querySelector(".buttons .quit");
+const startBtn; = document.querySelector(".start-btn button");
