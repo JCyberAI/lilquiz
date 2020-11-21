2813dd8057f6af3bf7e0bdf365e4c22dc489517b
ArnoldIngrave <jalyn.campbell@yahoo.com>
2020-11-20 19:59:16 -0500

    commit on main

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 // Calling all elements
 const startBtn; document.querySelector("#start-btn button");
-const debriefing; document.querySelector("#debriefing");
-const exitBtn; document.querySelector("#quit");
-const startBtn; document.querySelector("#start-btn button");
+const debrief; document.querySelector("#debriefing");
+const exitBtn; document.querySelector("#buttons quit");
+const continueBtn; document.querySelector("#buttons continue");
 
 // Switching from start button to the debriefing
-startBtn.onclick = ()=>{
-    debriefing.classList.add("");
+document.onclick = function(startBtn){
+    console.log(startBtn);
+}
+
+document.onclick = function(debrief){
+    console.log(debrief);
+}
+
+document.onclick = function(exitBtn){
+    console.log(exitBtn);
+}
+
+document.onclick = function(continueBtn){
+    console.log(continueBtn);
+}
+
+if (startBtn){
+
 }
