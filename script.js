535a2b04878c4fcd060f142a7d4511cbaa50f6f3
ArnoldIngrave <jalyn.campbell@yahoo.com>
2020-11-21 17:47:46 -0500

    commit on main

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ var debrief = document.querySelector("#debriefing");
 var exitBtn = document.querySelector("#buttons quit");
 var continueBtn = document.querySelector("#buttons continue");
 
+//When Start Button Clicked, show the debrief box
+if (startBtn) {
+    debrief.add("activeInfo");
+} else {
+    console.log(startBtn);
+}
+
+//When Exit Button Clicked, hide the debrief box
+if (exitBtn) {
+    debrief.add("activeInfo");
+} else {
+    console.log(exitBtn);
+}
+
+//When Continue Button Clicked, 
+if (continueBtn) {
+    debrief.add("aciveInfo");
+}
