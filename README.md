7101fb33654fe5b11f72bd8c4453d4701c4b7545
ArnoldIngrave <jalyn.campbell@yahoo.com>
2020-11-21 17:52:22 -0500

    commit on main

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # lilquiz
+
+I was able to format the entire quiz and even get the divs to be hidden behind each other using z index for CSS. i went to start the Java script and my first page 
--- script.js
+++ script.js
@@ -6,19 +6,19 @@ var continueBtn = document.querySelector("#buttons continue");
 
 //When Start Button Clicked, show the debrief box
 if (startBtn) {
-    debrief.add("activeInfo");
+    debrief.add("debriefing-list");
 } else {
     console.log(startBtn);
 }
 
 //When Exit Button Clicked, hide the debrief box
 if (exitBtn) {
-    debrief.add("activeInfo");
+    debrief.remove("debriefing-list");
 } else {
     console.log(exitBtn);
 }
 
-//When Continue Button Clicked, 
+//When Continue Button Clicked, show the quiz box
 if (continueBtn) {
-    debrief.add("aciveInfo");
+    debrief.add("quiz-box");
 }
