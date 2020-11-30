16c30e45d41598149ad8c45510eb4173d15b1ab9
ArnoldIngrave <jalyn.campbell@yahoo.com>
2020-11-30 17:15:56 -0500

    commit on main

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
 // Calling all elements
-var startBtn = document.querySelector("#start-btn button");
-var debrief = document.querySelector("#debriefing");
-var exitBtn = document.querySelector("#buttons quit");
-var continueBtn = document.querySelector("#buttons continue");
+const A = document.querySelector("startBtn button");
+const B = document.querySelector("debriefing");
+const C = document.querySelector("buttons quit");
+const D = document.querySelector("buttons continue");
 
 //When Start Button Clicked, show the debrief box
-if (startBtn) {
-    debrief.add("debriefing-list");
-} else {
-    console.log(startBtn);
-}
-
-//When Exit Button Clicked, hide the debrief box
-if (exitBtn) {
-    debrief.remove("debriefing-list");
-} else {
-    console.log(exitBtn);
-}
-
-//When Continue Button Clicked, show the quiz box
-if (continueBtn) {
-    debrief.add("quiz-box");
-}
