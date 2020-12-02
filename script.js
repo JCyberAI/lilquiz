e2446137390ea3d0ae9d40895134f1e13a162c4c
ArnoldIngrave <jalyn.campbell@yahoo.com>
2020-12-01 21:15:39 -0500

    commit on main

--- script.js
+++
@@ -1,7 +0,0 @@
-// Calling all elements
-const A = document.querySelector("div > startBtn");
-const B = document.querySelector("div > debriefing");
-const C = document.querySelector("div > buttons.quit");
-const D = document.querySelector("div > buttons.continue");
-
-//When Start Button Clicked, show the debrief box
