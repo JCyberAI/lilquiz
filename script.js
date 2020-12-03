36be39c7d79a658f987335869f264967e7b0a066
ArnoldIngrave <jalyn.campbell@yahoo.com>
2020-12-02 21:35:43 -0500

    commit on main

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-/*
+//Declareing variables of certian HTML classes (in Divs) & elements
+
 var startBtn = document.querySelectorAll("div.startBtn, button");
 var rules = document.querySelectorAll("div.debriefing, div.debriefingTitle, div.debriefingList, div.buttons, ");
 var quizBox = document.querySelectorAll("div.quizBox, header, section, footer");
 var results = document.querySelectorAll("div.icon, div.completeText, div.endButtons");
 
-*/
+//Delcaring an array for the variables 
+
+var = ["startBtn", "rules", "quizBox", "results"];
+
+//Declaring a function to go through the variables
+
+function boxedContent {
+    
+}
