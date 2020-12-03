0fd38267aeb699c432dff43ead7a8b97fd16ab18
ArnoldIngrave <jalyn.campbell@yahoo.com>
2020-12-02 21:22:27 -0500

    commit on main

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*
 var startBtn = document.querySelectorAll("div.startBtn, button");
 var rules = document.querySelectorAll("div.debriefing, div.debriefingTitle, div.debriefingList, div.buttons, ");
-var quizBox = document.querySelectorAll("div.quizBox, div.title, div.timer");
-var 
+var quizBox = document.querySelectorAll("div.quizBox, header, section, footer");
+var results = document.querySelectorAll("div.icon, div.completeText, div.endButtons");
+
 */
