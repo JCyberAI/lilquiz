44a6dff134220d93db1d0169207079a142f9a489
Jalyn Campbell <jalyn.campbell@yahoo.com>
2021-02-28 15:52:06 -0500

    Changes to script

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
 //Declareing variables of certian HTML classes (in Divs) & elements
 
-//var startBtn = document.querySelectorAll(".startBtn");
+var startBtn = document.querySelectorAll("startBtn");
 var rules = document.querySelectorAll(".debriefing");
 var quizBox = document.querySelectorAll(".quizBox");
 var resultBox = document.querySelectorAll(".resultBox");
+var timmerSec = document.querySelectorAll("quizBox, header, timer, timerSec");
+var timer = document.getElementById("timer");
+var secondsLeft = 30;
 
-//Delcaring an array for the variables 
 
-var bodyContent = ["startBtn", "rules", "quizBox", "resultBox"];
-
-// Declaring the function
+// Declaring the functions
 boxedContent()
 
 //Declaring a function to go through the array
 
-var hold = function boxedContent () {
+function boxedContent() {
+
+    var bodyContent = ["startBtn", "rules", "quizBox", "resultBox"];
 
     for (i = 0; i < bodyContent.length; i++) {
-    console.log(boxedContent);
+        document.getElementsByClassName("startBtn").getElementsByClassName("activeInfo");
     }
-    document.getElementsByClassName("startBtn").onclick = function otherContent() {
-    document.getElementsByClassName("startBtn").getElementsByClassName("activeInfo");
 
-    }
 }
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerSec.textContent = secondsLeft + " seconds left";
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+
+    }, 1000);
+}
+
+function sendMessage() {
+    timerSec.textContent = " ";
+
+    var message = document.createElement("div");
+
+    message.setAttribute("alert", "Your time is up!");
+    timer.appendChild(message);
+
+}
+
+setTime();
 
