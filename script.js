f9c20f627f02e67850f83ebb37e77d4580d737fb
Jalyn Campbell <jalyn.campbell@yahoo.com>
2021-03-01 18:00:18 -0500

    refactoring

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
 //Declareing variables of certian HTML classes (in Divs) & elements
 
 var startBtn = document.querySelector("startBtn");
-var rules = document.querySelector(".debriefing");
-var quizBox = document.querySelector(".quizBox");
-var resultBox = document.querySelector(".resultBox");
 var timmerSec = document.querySelector("timerSec");
 var timer = document.getElementById("timer");
 var secondsLeft = 30;
 
-
-// Declaring the functions
-boxedContent();
-setTime();
-
-//Declaring a function to go through the array
-
-function boxedContent() {
-
-    var bodyContent = [startBtn, rules, quizBox, resultBox];
-
-    for (i = 0; i < bodyContent.length; i++) {
-        startBtn.appendChild("activeInfo");
-    }
-
-}
 
 function setTime() {
     var timerInterval = setInterval(function () {
@@ -48,3 +29,5 @@ function sendMessage() {
 
 }
 
+// Declaring the functions
+setTime();
