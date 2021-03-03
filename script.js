4f98b2838aae968759baf43afe809655cdca3ab4
Jalyn Campbell <jalyn.campbell@yahoo.com>
2021-03-03 17:54:48 -0500

    tutor assisted code

--- script.js
+++ script.js
@@ -6,22 +6,85 @@ var quizBox = document.querySelector(".quizBox");
 var resultBox = document.querySelector(".resultBox");
 var timmerSec = document.querySelector("timerSec");
 var timer = document.getElementById("timer");
+var quetext = document.getElementById("queText");
+var choices = document.getElementById("choices");
+var currentindex = 0
 var secondsLeft = 30;
+var timerId;
+
+var questions = [
+
+    {
+        title: "What does CSS stand for?",
+        choices: ["Cake Salad Squirrels", "Camel Sips Silence", "Cascading Style Sheets"],
+        answer: "Cascading Style Sheets"
+    },
+
+    {
+        title: "How do you call classes in CSS?",
+        choices: ["$", "#", ".", "&"],
+        answer: "."
+    },
+
+    {
+        title: "When creating a function, what symbols must be used before the curly brackets?",
+        choices: ["()", "{}", "[]"],
+        answer: "()"
+    },
+
+    {
+        title: "How does memory get stored on the client side?",
+        choices: ["Local Storage", "Back Pocket", "Console", "In the oven"],
+        answer: "Local Storage"
+    },
+
+    {
+        title: "Where do APIs operate within a full stack application?",
+        choices: ["Server", "Middleware", "Client-side", "All the above"],
+        answer: "All the above"
+    },
+
+    {
+        title: "What does 'npm i' do?",
+        choices: ["Installs data", "Initiates a sequence", "Installs dependancies", "Blows up the computer"],
+        answer: "Installs dependancies"
+    }
+
+]
 
 
 // Declaring the functions
 boxedContent();
 setTime();
 
-//Declaring a function to go through the array
 
-function boxedContent() {
+function startQuiz () {
 
-    var bodyContent = [startBtn, rules, quizBox, resultBox];
+    var startscreen = document.getElementById("startscreen");
+    startscreen.setAttribute("class", "hide");
+    timer.textContent = secondsLeft;
+    timerId = setInterval(setTime, 1000)
+    quetext.removeAttribute("class")
 
-    for (i = 0; i < bodyContent.length; i++) {
-        startBtn.appendChild("activeInfo");
-    }
+    questions();
+}
+
+function questions () {
+
+    var currentquestion = questions[currentindex]
+    quetext.textContent = currentquestion.title
+    choices.innerHTML = ""
+
+    currentquestion.choices.forEach (function (choice, i) {
+
+        var choiceBtn = document.createElement ("button")
+        choiceBtn.setAttribute("class", "choice")
+        choiceBtn.setAttribute("value", choice)
+        choiceBtn.textContent = i + 1 + " " + choice
+        choiceBtn.onclick = verifyAnswer
+        choices.appendChild(choiceBtn)
+
+    })
 
 }
 
@@ -48,3 +111,4 @@ function sendMessage() {
 
 }
 
+startBtn.onclick = startQuiz;
