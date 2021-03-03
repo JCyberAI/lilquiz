b95e2c4c36f6a33c2f8a60a13479963a1baa771b
Jalyn Campbell <jalyn.campbell@yahoo.com>
2021-03-03 16:42:16 -0500

    Revert "refactoring"
    This reverts commit f9c20f627f02e67850f83ebb37e77d4580d737fb.

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 //Declareing variables of certian HTML classes (in Divs) & elements
 
 var startBtn = document.querySelector("startBtn");
+var rules = document.querySelector(".debriefing");
+var quizBox = document.querySelector(".quizBox");
+var resultBox = document.querySelector(".resultBox");
 var timmerSec = document.querySelector("timerSec");
 var timer = document.getElementById("timer");
 var secondsLeft = 30;
 
+
+// Declaring the functions
+boxedContent();
+setTime();
+
+//Declaring a function to go through the array
+
+function boxedContent() {
+
+    var bodyContent = [startBtn, rules, quizBox, resultBox];
+
+    for (i = 0; i < bodyContent.length; i++) {
+        startBtn.appendChild("activeInfo");
+    }
+
+}
 
 function setTime() {
     var timerInterval = setInterval(function () {
@@ -29,5 +48,3 @@ function sendMessage() {
 
 }
 
-// Declaring the functions
-setTime();
